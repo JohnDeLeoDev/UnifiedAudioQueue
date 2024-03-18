71e7e2c2713430a1154b7a487a9ae25f5dc5ddcf
John DeLeo <jdeleo@fastmail.com>
2024-03-18 09:35:52 -0400

    Initial Commit

---
+++ UnifiedAudioQueue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnifiedAudioQueue
+//
+//  Created by John C. DeLeo on 3/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnifiedAudioQueue/UnifiedAudioQueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnifiedAudioQueueApp.swift
+//  UnifiedAudioQueue
+//
+//  Created by John C. DeLeo on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct UnifiedAudioQueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
